5bad81fb34759622d354fa97ec61c61ac82d8f06
Euripedes <=>
2020-10-10 03:07:17 -0300

    Update maneira_1_aritmetica_com_numeros_binarios.cs

--- Trocar variáveis/maneira_1_aritmetica_com_numeros_binarios.cs
+++ Trocar variáveis/maneira_1_aritmetica_com_numeros_binarios.cs
@@ -1,15 +1,15 @@
 using System;
 
-//A troca aritmética de números dispensa o uso de uma terceira variável auxiliar. Ao final da execução, mostra os dois valores no console.
+//A troca aritmética de números dispensa o uso de uma terceira variável auxiliar. Os valores dessas variávais estão restritos ao conjunto dos números inteiros.
 
 class TrocaAritmeticaDeNumerosBinarios
 {
     public static void Main()
     {
-        Console.Write("Digite o valor do primeiro número: ");
+        Console.Write("Digite o valor do primeiro número inteiro: ");
         int n1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("\nDigite o valor do segundo número: ");
+        Console.Write("\nDigite o valor do segundo número inteiro: ");
         int n2 = Convert.ToInt32(Console.ReadLine());
 
         n1 = n1 ^ n2;
